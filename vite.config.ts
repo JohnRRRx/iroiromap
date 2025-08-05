820deb6343b3f7679546dc1c920fa679594b47d6
“JohnRRRx” <tom043838@gmail.com>
2025-08-05 13:01:52 +0900

    remove: allowhost and related Render deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    host:'0.0.0.0',
-    port: 6666,
-    allowedHosts: ['moving-0yu1.onrender.com'],
-  
-  }
 })
 
 
